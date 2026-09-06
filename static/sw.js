8365562b88ad4c7bfac4d8e50f8e48752dbea86c
martinez021593-sudo <martinez021593@gmail.com>
2026-09-06 00:52:21 -0500

    Delete static directory

--- static/sw.js
+++
@@ -1,9 +0,0 @@
-
-const CACHE="routeops-v02-shell-v1";
-const SHELL=["/static/style.css","/static/app.js","/manifest.webmanifest"];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
-self.addEventListener("activate",event=>event.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",event=>{
-  if(event.request.method!=="GET") return;
-  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
-});
